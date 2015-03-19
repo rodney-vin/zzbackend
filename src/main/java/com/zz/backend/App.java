f8a009f40c7545f9dd0c71e3f62e32ce6b38038f
jncz <li.j2ee@gmail.com>
2015-03-19 15:28:52 +0800

    initial

---
+++ src/main/java/com/zz/backend/App.java
@@ -0,0 +1,13 @@
+package com.zz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+
+}
